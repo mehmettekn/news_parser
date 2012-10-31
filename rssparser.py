56ccd1e0443e9757c25e35fd97d70d5f2a88b449
Mehmet <mehmettekn@ubuntu.(none)>
2012-10-31 10:40:46 +0200

    third commit

--- rssparser.py
+++ rssparser.py
@@ -1,34 +1,31 @@
 from main import *
-from bs4 import BeautifulSoup
-from google.appengine.api import urlfetch
-from xml.dom import minidom
-from collections import namedtuple
-from time import strptime, mktime
-from datetime import datetime
-from dateutil import parser
-
-urllist = {"http://www.haberturk.com/rss":{}, 
-           "http://www.milliyet.com.tr/D/rss/rss/RssSD.xml":{},
-           "http://rss.hurriyet.com.tr/rss.aspx?sectionId=1":{},
-           "http://www.sabah.com.tr/rss/SonDakika.xml":{},
-           "http://www.radikal.com.tr/d/rss/RssSD.xml":{}}
-
-no_image_url = "https://encrypted-tbn1.gstatic.com/images?      q=tbn:ANd9GcTEzsZdA0I-mk9lPYUZaJbJ6rcTYXl53Rv1uin78SRlckU9MwV6Kw"
+from urls import *
+from post import *
 
 class RssParser(BaseHandler):
     def get_data(self, name, src):
-        _data = src.getElementsByTagName(name)[0]
-        data = _data.lastChild.data
+        try:        
+            _data = src.findAll(name).pop()  
+        except(IndexError):
+            _data = BeautifulSoup("No Data Received") 
+        data = _data.get_text()
         return data    
-
-    def parse_rss(self):
+            
+    def _parse_rss(self, urltype):
         result = namedtuple('Result', ['title', 'description', 'link',
-                                   'content', 'image', 'pubDate', 'src'])
+                                       'content', 'image', 'pubDate',
+                                       'src', 'newstype'])
         results = []
-        for url in urllist:
-            page = urlfetch.fetch(url).content
-            xml = minidom.parseString(page)
-            posts = xml.getElementsByTagName("item")
+        for url in urllist[urltype]:            
+            newstype = urltype                
+            logging.error(url)                    
+            try:                
+                _page = urlfetch.fetch(url)  
+            except urlfetch.DeadlineExceededError:
+                 continue                
+            page = _page.content                
+            xml = BeautifulSoup(page)
+            posts = xml.findAll("item")
             source = self.get_data("title", xml)            
             for post in posts:
                 title = self.get_data("title", post)
@@ -37,12 +34,31 @@ class RssParser(BaseHandler):
                 link = self.get_data("link", post)
                 _image = _desc.find('img')
                 _pubDate = self.get_data('pubDate', post)
-                pubDate = parser.parse(_pubDate)             
+                try:
+                    pubDate = parser.parse(_pubDate) + timedelta(hours=1)           
+                except (ValueError):
+                    pubDate = datetime.utcnow()  
                 if _image: image = _image['src']
                 else: image = no_image_url
                 content = "no content yet" #add parsing for content later"
                 src = source      
-                results.append(result(title, desc, link, content, image, pubDate, src))
-        return results 
-            
- 
+                results.append(result(title, desc, link, content, image, pubDate, src, newstype))
+        return results
+        
+    def parse_rss(self, urltype):
+        results = self._parse_rss(urltype)
+        for r in results:
+            val1 = memcache.get(r.link)                  
+            val2 = memcache.get(r.image)            
+            if val1 == None and val2 == None:
+                get_posts(update = True)                
+                logging.error("New Post Entity Being Created")                
+                p = Post(parent = post_key(), title = r.title,
+                         description = r.description,
+                         link = r.link, content = r.content,
+                         image = r.image, pubDate = r.pubDate,
+                         src = r.src)
+                p.put()
+                memcache.set(r.link, True)
+                memcache.set(r.image, True)
+            else: logging.error("Duplicate post avoided")
